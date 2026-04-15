4052d1fa5461c6e6e85bf9f268090c10962cdd75
Gaetan <george.gaetan@gmail.com>
2026-04-15 14:23:37 +0200

    feat(reverb): event LobbyGameCompleted

---
+++ app/Events/LobbyGameCompleted.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+final class LobbyGameCompleted implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(
+        public readonly string $lobbyId,
+        public readonly array  $leaderboard,
+    ) {}
+
+    public function broadcastOn(): Channel
+    {
+        return new PresenceChannel('lobby.' . $this->lobbyId);
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'game.completed';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'leaderboard' => $this->leaderboard,
+        ];
+    }
+}
